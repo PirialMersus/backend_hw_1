25fb2d0903486aa308c57d16f054354031d50070
pirialmersus <pirialmersus@gmail.com>
2023-07-24 10:52:38 +0300

    before pushing to versel

--- src/index.ts
+++ src/index.ts
@@ -22,22 +22,31 @@ type VideoType = {
   minAgeRestriction: number | null,
   createdAt: string,
   publicationDate: string,
-  availableResolutions: AvailableResolutions[]
+  availableResolutions: AvailableResolutions[] | null,
 }
 
-const videos: VideoType[] = [
-  {
-    id: 0,
-    title: "string",
-    author: "string",
-    canBeDownloaded: true,
-    minAgeRestriction: null,
-    createdAt: "2023-07-17T15:50:40.497Z",
-    publicationDate: "2023-07-17T15:50:40.497Z",
-    availableResolutions: [
-      AvailableResolutions.P144
-    ]
-  }
+type ErrorMessageType = {
+  message: string,
+  field: string
+}
+
+type ErrorType = {
+  errorMessages: ErrorMessageType[]
+}
+
+let videos: VideoType[] = [
+  // {
+  //   id: 0,
+  //   title: "string",
+  //   author: "string",
+  //   canBeDownloaded: true,
+  //   minAgeRestriction: null,
+  //   createdAt: "2023-07-17T15:50:40.497Z",
+  //   publicationDate: "2023-07-17T15:50:40.497Z",
+  //   availableResolutions: [
+  //     AvailableResolutions.P144
+  //   ]
+  // }
 ]
 
 const app = express()
@@ -51,24 +60,39 @@ app.get('/videos', (req: Request, res: Response) => {
 
 app.get('/videos/:id', (req: RequestWithParams<{ id: number }>, res: Response) => {
   const id = +req.params.id
-  const video = videos.find(video => video.id === id)
+  const foundVideo = videos.find(video => video.id === id)
 
-  if (!video) {
-    res.sendStatus(404)
+  if (foundVideo) {
+    res.send(foundVideo).status(204)
     return
   }
 
-  res.send(video)
+  res.sendStatus(404)
+})
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+  videos.length = 0
+
+  res.sendStatus(204)
 })
 
-type ErrorMessageType = {
-  message: string,
-  field: string
-}
+app.delete('/videos/:id', (req: RequestWithParams<{ id: number }>, res: Response) => {
+  const id = +req.params.id
+  let foundVideoExist = false
 
-type ErrorType = {
-  errorMessages: ErrorMessageType[]
-}
+  const filteredVideos = videos.filter(video => {
+    if (video.id !== id) {
+      foundVideoExist = true
+    }
+    return video.id !== id
+  })
+  if (foundVideoExist) {
+    videos = filteredVideos
+    res.sendStatus(204)
+    return
+  }
+  res.sendStatus(404)
+})
+
 
 app.post('/videos', (req: RequestWithBody<{
   title: string,
@@ -89,12 +113,15 @@ app.post('/videos', (req: RequestWithBody<{
   }
   if (Array.isArray(availableResolutions)) {
     availableResolutions.map(r => {
-      !AvailableResolutions[r] && errors.errorMessages.push({message: 'Invalid availableResolutions', field: 'availableResolutions'})
+      !AvailableResolutions[r] && errors.errorMessages.push({
+        message: 'Invalid availableResolutions',
+        field: 'availableResolutions'
+      })
     })
   } else {
     availableResolutions = []
   }
-  if(errors.errorMessages.length){
+  if (errors.errorMessages.length) {
     res.status(400).send(errors)
   }
   const createdAt = new Date()
@@ -116,6 +143,72 @@ app.post('/videos', (req: RequestWithBody<{
 
   res.status(201).send(newVideo)
 })
+app.put('/videos/:id', (req: RequestWithParams<{ id: number }> & RequestWithBody<{
+  title: string,
+  author: string,
+  availableResolutions: AvailableResolutions[],
+  canBeDownloaded: boolean,
+  minAgeRestriction: string,
+  publicationDate: string,
+}>, res: Response) => {
+  let errors: ErrorType = {
+    errorMessages: []
+  }
+  const id = +req.params.id
+  console.log('id ', id)
+  let {title, author, availableResolutions, canBeDownloaded, minAgeRestriction, publicationDate} = req.body
+  const availableResolutionsExists = Array.isArray(availableResolutions)
+  const minAgeRestrictionValue = minAgeRestriction?.trim() ?? '';
+
+  if (publicationDate && isNaN(Date.parse(publicationDate))) {
+    errors.errorMessages.push({message: 'Invalid publicationDate', field: 'publicationDate'});
+  }
+  console.log('minAgeRestriction ', minAgeRestriction)
+
+  if (!!minAgeRestrictionValue && (isNaN(+minAgeRestrictionValue) || minAgeRestrictionValue.length < 1 || minAgeRestrictionValue.length > 18)) {
+    errors.errorMessages.push({message: 'Invalid minAgeRestriction', field: 'minAgeRestriction'});
+  }
+
+  if (!title || !title.length || title.trim().length > 40) {
+    errors.errorMessages.push({message: 'Invalid title', field: 'title'})
+  }
+
+  if (!author || !author.length || author.trim().length > 20) {
+    errors.errorMessages.push({message: 'Invalid author', field: 'author'})
+  }
+  if (availableResolutionsExists) {
+    availableResolutions.map(r => {
+      !AvailableResolutions[r] && errors.errorMessages.push({
+        message: 'Invalid availableResolutions',
+        field: 'availableResolutions'
+      })
+    })
+  }
+  if (errors.errorMessages.length) {
+    res.status(400).send(errors)
+    return;
+  }
+
+  const foundVideo = videos.find(video => video.id === id)
+  if (foundVideo) {
+    if (canBeDownloaded) {
+      foundVideo.canBeDownloaded = canBeDownloaded
+    }
+    if (minAgeRestriction) {
+      foundVideo.minAgeRestriction = +minAgeRestriction || null
+    }
+    if (availableResolutionsExists) {
+      foundVideo.availableResolutions = availableResolutions || null
+    }
+    foundVideo.title = title;
+    foundVideo.minAgeRestriction = +minAgeRestriction || null;
+    foundVideo.author = author;
+    foundVideo.publicationDate = publicationDate;
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
